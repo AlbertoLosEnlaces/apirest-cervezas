70d67a74798460cd3091b09cc6d4c859da5a1d67
AlbertoLosEnlaces <janunezlosenlaces@gmail.com>
2016-09-15 13:41:23 +0200

    nodejs app con routes

--- app/server.js
+++ app/server.js
@@ -1,34 +1,17 @@
 var express = require('express') //llamamos a Express
-var app = express()   //en app tengo el objeto que representa al servidor express               
+var app = express()       
+var bodyParser = require('body-parser')        
 
 var port = process.env.PORT || 8080  // establecemos nuestro puerto
 
-// para establecer las distintas rutas, necesitamos instanciar el express router
-var router = express.Router()             
-
-//establecemos nuestra primera ruta, mediante get.
-router.get('/', function(req, res) {
-  res.json({ mensaje: '¡Bienvenido a nuestra API!' })  
-})
-
-//body-parser es un middleware
-var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-router.post('/',function(req,res) { 
-  res.json({mensaje: req.body.nombre})  
-})
-
-
-router.get('/:nombre', function(req, res) {
-  res.json({ mensaje: '¡Hola' + req.params.nombre })   
-})
+app.use(bodyParser.json())            
 
 // nuestra ruta irá en http://localhost:8080/api
 // es bueno que haya un prefijo, sobre todo por el tema de versiones de la API
+var router = require('./routes')
 app.use('/api', router)
 
-// iniciamos nuestro servidor
+//arrancamos el servidor
 app.listen(port)
 console.log('API escuchando en el puerto ' + port)
